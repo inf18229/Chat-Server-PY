864d3a096c5b531de18b0ffb98e81ad9420fb176
Stefan Maier <inf18229@lehre.dhbw-stuttgart.de>
2021-03-16 20:53:33 +0100

    client server

--- server_new.py
+++ server_new.py
@@ -1,23 +1,26 @@
-import socket,threading
+import socket
+from _thread import *
 from headerMessage import *
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+ThreadCount = 0
 
 class ChatServer:
     def __init__(self):
         self.server_socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((HOST,PORT))
-        self.server_socket.listen()
+        self.server_socket.listen(5)
         self.registeredClient = []
     def getserverSocket(self):
         return self.server_socket
     def recieveData(self,connection):
-        acceptMessageHeader = connection.recv(HEADER_SIZE)
-        if acceptMessageHeader:
-            acceptMessageHeader = int(acceptMessageHeader.strip())
-            acceptMessage = connection.recv(acceptMessageHeader)
-            print(acceptMessage.decode('utf-8'))
+        while True:
+            acceptMessageHeader = connection.recv(HEADER_SIZE)
+            if acceptMessageHeader:
+                acceptMessageHeader = int(acceptMessageHeader.strip())
+                acceptMessage = connection.recv(acceptMessageHeader)
+                print(acceptMessage.decode('utf-8'))
     def registerClient(self,connection):
         self.registeredClient.append(connection)
     def getregisteredClients(self):
@@ -29,12 +32,7 @@ if __name__=="__main__":
         print("Warte auf Verbindung")
         connection, address = MainChatServer.getserverSocket().accept()
         #MainChatServer.registerClient(connection
-        print("Verbindung akzpetiert")
-        while True:
-            ChatThread = threading.Thread(target=MainChatServer.recieveData(connection),daemon=True)
-            ChatThread.start()
-            print("Thread startet")
-            ChatThread.join()
-            break
-            #MainChatServer.recieveData(connection)
+        start_new_thread(MainChatServer.recieveData,(connection,))
+        ThreadCount += 1
+        print('Thread Number: ' + str(ThreadCount))
 

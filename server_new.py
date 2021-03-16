1199a5b824febab563c73d26511b07e730000d14
Stefan Maier <inf18229@lehre.dhbw-stuttgart.de>
2021-03-16 15:48:59 +0100

    added some threading Test

--- server_new.py
+++ server_new.py
@@ -9,23 +9,32 @@ class ChatServer:
         self.server_socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((HOST,PORT))
         self.server_socket.listen()
+        self.registeredClient = []
     def getserverSocket(self):
         return self.server_socket
     def recieveData(self,connection):
-        while True:
-            acceptMessageHeader = connection.recv(HEADER_SIZE)
-            if acceptMessageHeader:
-                acceptMessageHeader = int(acceptMessageHeader.strip())
-                acceptMessage = connection.recv(acceptMessageHeader)
-                print(acceptMessage.decode('utf-8'))
+        acceptMessageHeader = connection.recv(HEADER_SIZE)
+        if acceptMessageHeader:
+            acceptMessageHeader = int(acceptMessageHeader.strip())
+            acceptMessage = connection.recv(acceptMessageHeader)
+            print(acceptMessage.decode('utf-8'))
+    def registerClient(self,connection):
+        self.registeredClient.append(connection)
+    def getregisteredClients(self):
+        return self.registeredClient
     
 if __name__=="__main__":
     MainChatServer = ChatServer()
     while True:
+        print("Warte auf Verbindung")
         connection, address = MainChatServer.getserverSocket().accept()
+        #MainChatServer.registerClient(connection
         print("Verbindung akzpetiert")
-        #chatThread = threading.Thread(target=MainChatServer.recieveData(connection))
-        #chatThread.start()
-        #chatThread.join()
-        MainChatServer.recieveData(connection)
+        while True:
+            ChatThread = threading.Thread(target=MainChatServer.recieveData(connection),daemon=True)
+            ChatThread.start()
+            print("Thread startet")
+            ChatThread.join()
+            break
+            #MainChatServer.recieveData(connection)
 

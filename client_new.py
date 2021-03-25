c06880c4d2b6149a107bc16766240fb45184aabb
Stefan Maier <inf18229@lehre.dhbw-stuttgart.de>
2021-03-25 11:50:48 +0100

    TLS/SSL init

--- client_new.py
+++ client_new.py
@@ -1,17 +1,21 @@
 from headerMessage import format_message
 import socket
+import ssl
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 class ChatClient:
     def __init__(self):
-        self.client_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((HOST, PORT))
+        self.context = ""
+        self.context = ssl.create_default_context()
+        self.context.load_verify_locations('/home/stefanmaier/Dokumente/Privat/cert/server.pem')
+        self.client_socket =  socket.create_connection((HOST,PORT))
+        self.client_socket = self.context.wrap_socket(self.client_socket, server_hostname=HOST)
     def writeMessage(self):
         while True:
             nachricht = input("Nachricht\n")
-            self.client_socket.sendall(bytes(format_message(nachricht),'utf-8'))
+            self.client_socket.send(bytes(format_message(nachricht),'utf-8'))
 
 if __name__ == "__main__":
     MainChatClient = ChatClient()
--- server_new.py
+++ server_new.py
@@ -1,4 +1,5 @@
 import socket
+import ssl
 from _thread import start_new_thread
 from headerMessage import HEADER_SIZE
 
@@ -8,9 +9,14 @@ ThreadCount = 0
 
 class ChatServer:
     def __init__(self):
+        self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        self.context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        self.context.load_cert_chain(certfile='/home/stefanmaier/Dokumente/Privat/cert/server.crt', keyfile='/home/stefanmaier/Dokumente/Privat/cert/server.key')
+
         self.server_socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((HOST,PORT))
         self.server_socket.listen(5)
+        self.server_socket = self.context.wrap_socket(self.server_socket,server_side=True)
         self.registeredClient = []
     def getserverSocket(self):
         return self.server_socket

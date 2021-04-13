c8d37b98715875ca953498295e441f52f468e9c0
Stefan Maier <inf18229@lehre.dhbw-stuttgart.de>
2021-04-13 16:38:57 +0200

    Added SQLite DB

--- server_new.py
+++ server_new.py
@@ -1,6 +1,7 @@
 import socket
 from _thread import start_new_thread
 from headerMessage import HEADER_SIZE
+from server_sql import SQLServer
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
@@ -12,6 +13,7 @@ class ChatServer:
         self.server_socket.bind((HOST,PORT))
         self.server_socket.listen(5)
         self.registeredClient = []
+        self.UserDB = SQLServer()
     def getserverSocket(self):
         return self.server_socket
     def recieveData(self,connection,address):
@@ -21,17 +23,21 @@ class ChatServer:
                 acceptMessageHeader = int(acceptMessageHeader.strip())
                 acceptMessage = connection.recv(acceptMessageHeader)
                 print(address[0],":",address[1],"-->",acceptMessage.decode('utf-8'))
+                self.UserDB.getUserName("StefanM")
     def registerClient(self,connection):
+        self.UserDB.createUserinDB("StefanM","Stefan","Maier")
         self.registeredClient.append(connection)
     def getregisteredClients(self):
         return self.registeredClient
+    def __del__(self):
+        self.server_socket.close()
     
 if __name__=="__main__":
     MainChatServer = ChatServer()
     while True:
         print("Warte auf Verbindung")
         connection, address = MainChatServer.getserverSocket().accept()
-        #MainChatServer.registerClient(connection
+        MainChatServer.registerClient(connection)
         start_new_thread(MainChatServer.recieveData,(connection,address))
         ThreadCount += 1
         print('Thread Number: ' + str(ThreadCount))
---
+++ server_sql.py
@@ -0,0 +1,47 @@
+import sqlite3
+class SQLServer:
+    def __init__(self):
+        self.userDB_Connection = sqlite3.connect("db/User.db")
+        self.userDB_Cursor = self.userDB_Connection.cursor()
+        # Create Basic Tables in SQL Database
+        self.userDB_Cursor.execute("""
+        SELECT count(name)
+        FROM sqlite_master
+        WHERE type='table' AND name='users'
+        """)
+        if self.userDB_Cursor.fetchall()[0]==0:
+
+            sql_CreateTables = """
+                CREATE TABLE users (
+                UName VARCHAR(20),
+                Vorname VARCHAR(20),
+                NACHNAME VARCHAR(20)
+                );"""
+            self.userDB_Cursor.execute(sql_CreateTables)
+            self.userDB_Connection.commit()
+        self.userDB_Connection.close()
+    def createUserinDB(self, userName, vName,nName):
+        DB_con = sqlite3.connect("db/User.db")
+        DB_curs = DB_con.cursor()
+        DB_curs.execute("""
+        INSERT INTO users 
+                VALUES (?,?,?)
+        """,
+        (userName, vName, nName))
+        DB_con.commit()
+        DB_con.close()
+
+    def getUserName(self,userName):
+        DB_con = sqlite3.connect("db/User.db")
+        DB_curs = DB_con.cursor()
+        DB_curs.execute("""
+        SELECT Vorname, NACHNAME
+        FROM users
+        WHERE UName = (?)
+        """,
+        (userName,))
+        print(DB_curs.fetchall())
+        DB_con.close()
+    def closeConnection(self):
+        #Only for destruction
+        self.userDB_Connection.close()

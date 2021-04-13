bf8818e89e7236983517f29ba93480416010bf48
Stefan Maier <inf18229@lehre.dhbw-stuttgart.de>
2021-04-13 16:40:49 +0200

    refactored ssl code

--- client_new.py
+++ client_new.py
@@ -6,72 +6,29 @@ import ssl
 import pprint
 import traceback
 
-HOST = "localhost"  # The server's hostname or IP address
+HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
-
-ssl_version = None
-certfile = None
-keyfile = "./ssl/key.pem"
-ciphers = None
-
-version_dict = {
-    "tlsv1.0" : ssl.PROTOCOL_TLSv1,
-    "tlsv1.1" : ssl.PROTOCOL_TLSv1_1,
-    "tlsv1.2" : ssl.PROTOCOL_TLSv1_2,
-    "sslv23"  : ssl.PROTOCOL_SSLv23,
-}
-
-
-def ssl_wrap_socket(sock, ssl_version=None, keyfile=None, certfile=None, ciphers=None):
-    try:
-        #2.init a sslContext with given version (if any)
-        if ssl_version is not None and ssl_version in version_dict:
-            #create a new SSL context with specified TLS version
-            sslContext = ssl.SSLContext(version_dict[ssl_version])
-            if option_test_switch == 1:
-                print("ssl_version loaded!! =", ssl_version)
-        else:
-            #default
-            sslContext = ssl.create_default_context()
-        
-        if ciphers is not None:
-            #if specified, set certain ciphersuite
-            sslContext.set_ciphers(ciphers)
-            if option_test_switch == 1:
-                print("ciphers loaded!! =", ciphers)
-        
-        #3. set root certificate path
-        if certfile is not None and keyfile is not None:
-            #if specified, load speficied certificate file and private key file
-            sslContext.verify_mode = ssl.CERT_REQUIRED
-            sslContext.check_hostname = True
-            sslContext.load_verify_locations(certfile, keyfile)
-            if option_test_switch == 1:
-                print("ssl loaded!! certfile=", certfile, "keyfile=", keyfile )
-            return sslContext.wrap_socket(sock, server_hostname = hostname)
-        else:
-            #default certs
-            sslContext.check_hostname = False
-            sslContext.verify_mode = ssl.CERT_NONE
-            sslContext.load_default_certs()
-            return sslContext.wrap_socket(sock)
-        
-    except ssl.SSLError:
-        print("wrap socket failed!")
-        print(traceback.format_exc())
-        sock.close()
-        sys.exit(-1)
 
 
 class ChatClient:
     def __init__(self):
         self.client_socket = socket(AF_INET,SOCK_STREAM)
-        self.client_socket = ssl_wrap_socket(self.client_socket,ssl_version, keyfile, certfile, ciphers)
+        try:
+            sslContext = ssl.create_default_context()
+            sslContext.check_hostname = False
+            sslContext.verify_mode = ssl.CERT_NONE
+            sslContext.load_default_certs()
+            self.client_socket = sslContext.wrap_socket(self.client_socket)
+        except ssl.SSLError:
+            print("wrap socket failed!")
+            print(traceback.format_exc())
+            sock.close()
+            sys.exit(-1)
         self.client_socket.connect((HOST, PORT))
     def writeMessage(self):
         while True:
             nachricht = input("Nachricht\n")
-            self.client_socket.sendall(bytes(format_message(nachricht),'utf-8'))
+            self.client_socket.send(bytes(format_message(nachricht),'utf-8'))
 
 if __name__ == "__main__":
     MainChatClient = ChatClient()
--- server_new.py
+++ server_new.py
@@ -10,40 +10,14 @@ HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 ThreadCount = 0
 
-ssl_version = None
 certfile = "./cert/certificate.pem"
 keyfile = "./cert/key.pem"
-ciphers = None
-
-version_dict = {
-    "tlsv1.0" : ssl.PROTOCOL_TLSv1,
-    "tlsv1.1" : ssl.PROTOCOL_TLSv1_1,
-    "tlsv1.2" : ssl.PROTOCOL_TLSv1_2,
-    "sslv23"  : ssl.PROTOCOL_SSLv23,
-}
 
 def ssl_wrap_socket(sock, ssl_version=None, keyfile=None, certfile=None, ciphers=None):
-
-    #1. init a context with given version(if any)
-    if ssl_version is not None and ssl_version in version_dict:
-        #create a new SSL context with specified TLS version
-        sslContext = ssl.SSLContext(version_dict[ssl_version])
-        if option_test_switch == 1:
-            print("ssl_version loaded!! =", ssl_version)
-    else:
-        #if not specified, default
-        sslContext = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-        
-    if ciphers is not None:
-        #if specified, set certain ciphersuite
-        sslContext.set_ciphers(ciphers)
-        if option_test_switch == 1:
-            print("ciphers loaded!! =", ciphers)
-    
+    sslContext = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)     
     #server-side must load certfile and keyfile, so no if-else
     sslContext.load_cert_chain(certfile, keyfile)
     print("ssl loaded!! certfile=", certfile, "keyfile=", keyfile)
-    
     try:
         return sslContext.wrap_socket(sock, server_side = True)
     except ssl.SSLError as e:
@@ -76,7 +50,7 @@ if __name__=="__main__":
     while True:
         print("Warte auf Verbindung")
         connection, address = MainChatServer.getserverSocket().accept()
-        connectionSocket = ssl_wrap_socket(connection,ssl_version,keyfile,certfile,ciphers)
+        connectionSocket = ssl_wrap_socket(connection,None,keyfile,certfile,None)
         start_new_thread(MainChatServer.recieveData,(connectionSocket,))
         ThreadCount += 1
         print('Thread Number: ' + str(ThreadCount))
